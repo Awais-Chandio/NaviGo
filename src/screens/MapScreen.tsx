a3e9a9f3b5e19ced7a585eb860b127acecc9cc15
Awais-Chandio <94171625+Awais-Chandio@users.noreply.github.com>
2026-07-20 18:01:06 +0500

    Updated

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,90 +1,89 @@
-import React, { useEffect } from "react";
-import { View, StyleSheet } from "react-native";
-import { Map, Camera, ViewAnnotation } from "@maplibre/maplibre-react-native"
-import { RequetLocationPermission } from "../permissions/locationPermission";
-
+import { useEffect,useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Map, Camera, ViewAnnotation } from '@maplibre/maplibre-react-native';
+import { RequestLocationPermission } from '../permissions/locationPermission';
+import Geolocation from 'react-native-geolocation-service';
 
 export default function MapScreen() {
-    const place = {
-        id: "1",
-        name: "Hyderabad",
-        lng: 68.3578,
-        lat: 25.3960,
-
-
+  type userLocationType={
+    coords:{
+        longitude:number;
+        latitude:number;
     }
-    async function getCurrentLocation() {
-        const status = await RequetLocationPermission()
-        if (!status) {
-            return;
-        }
+  }  
+  const [userLocation, setUserLocation] = useState<userLocationType | null>(null);
+  const place = {
+    id: '1',
+    name: 'Hyderabad',
+    lng: 68.3578,
+    lat: 25.396,
+  };
 
-
-
+  async function getCurrentLocation() {
+    const status = await RequestLocationPermission();
+    if (!status) {
+      return;
     }
+    Geolocation.getCurrentPosition((position)=>{
+        console.log(position)
+    }, (error)=>{
+        console.log(error.code, error.message)
+    },
+     {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 0,
+     }
+)
+  }
 
-    useEffect(() => {
-        getCurrentLocation()
-    }, [])
-    return (
-        <View style={Style.container}>
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
 
-
-            <Map
-                style={Style.map}
-                mapStyle={
-                    "https://tiles.openfreemap.org/styles/bright"
-                }
-
-
-            >
-                <Camera
-                    center={[68.3578, 25.3960]}
-                    zoom={14}
-                />
-                <ViewAnnotation
-                    id={place.id}
-                    lngLat={[place.lng, place.lat]}
-                >
-                    <View style={Style.markerContainer}>
-                        <View style={Style.markerDot} />
-                    </View>
-                </ViewAnnotation>
-
-
-
-            </Map>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Map
+        style={styles.map}
+        mapStyle={'https://tiles.openfreemap.org/styles/bright'}
+      >
+        <Camera center={[68.3578, 25.396]} zoom={14} />
+        <ViewAnnotation id={place.id} lngLat={[place.lng, place.lat]}>
+          <View style={styles.markerContainer}>
+            <View style={styles.markerDot} />
+          </View>
+        </ViewAnnotation>
+      </Map>
+    </View>
+  );
 }
-const Style = StyleSheet.create({
-    container: {
-        flex: 1,
 
-    },
-    map: {
-        flex: 1,
-    },
-    markerContainer: {
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#ac1818ff",
-        width: 30,
-        height: 30,
-        borderRadius: 15,
-        borderWidth: 2,
-        borderColor: "#cb2029ff",
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    markerDot: {
-        width: 12,
-        height: 12,
-        borderRadius: 6,
-        backgroundColor: "#130304ff",
-    },
-
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  markerContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ac1818ff',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: '#cb2029ff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  markerDot: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    backgroundColor: '#130304ff',
+  },
+});
